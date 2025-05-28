86ced89a0f92abcab39fc727e53a518609fd32e6
Ariel2711 <arielnaviandanaputra@gmail.com>
2025-05-28 10:31:51 +0700

    update security

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Session;
@@ -72,7 +73,17 @@ class AuthController extends Controller
 
     public function logout()
     {
-        Session::flush();
-        return redirect()->route('login_form')->with('message', 'Berhasil logout.');
+        try {
+            $response = Http::withToken(Session::get('api_token'))->post("{$this->apiBaseUrl}/logout");
+        } catch (ConnectionException $e) {
+            return redirect()->route('welcome')->with('message', 'Gagal logout. Silakan coba lagi.');
+        }
+
+        if ($response->status() == 200) {
+            Session::flush();
+            return redirect()->route('login_form')->with('message', 'Berhasil logout.');
+        } else {
+            return redirect()->route('welcome')->with('message', 'Gagal logout. Silakan coba lagi.');
+        }
     }
 }
